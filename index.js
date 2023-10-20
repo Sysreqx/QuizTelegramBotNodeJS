e98cf79f854160161a4175af0518f72979b18036
Kaisar Yerdenbekov <sysreqx@gmail.com>
2023-10-20 18:05:42 +0600

    feature(mongoDB): connect to DB, add a user to DB, e b a l v r o t v s e l o m a y e t s y a, pull a question from DB

--- index.js
+++ index.js
@@ -11,80 +11,148 @@ const mongoDB = config.get("mongoPass");
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
+const client = new MongoClient(mongoDB.toString());
 
-async function connect() {
-
-        const client = new MongoClient(mongoDB.toString());
+class DataBase {
+    async connect() {
 
         try {
             await client.connect();
-
-            console.log(await findOneQuestionById(client, 1));
-            console.log(await findAUserByChatId(client, 2323));
-
         } catch (e) {
             console.error(e);
-        } finally {
-            await client.close();
         }
+        // finally {
+        //     await client.close();
+        // }
 
         console.log("Connected to MongoDB");
 
-}
+    }
 
-connect().catch(console.error);
-
-
-async function findOneQuestionById(client, idOfQuestion) {
-    return await client.db("PMGQuizTelegramBotDB").collection("Questions").findOne({ id: idOfQuestion});
-}
+    async findOneQuestionById(client, idOfQuestion) {
+        return await client.db("PMGQuizTelegramBotDB").collection("Questions").findOne({ id: idOfQuestion});
+    }
 
 // return <null> if no
-async function findAUserByChatId(client, chatId) {
-    return await client.db("PMGQuizTelegramBotDB").collection("Users").findOne({ chat_id: chatId});
+    async findAUserByChatId(client, chatId) {
+        return await client.db("PMGQuizTelegramBotDB").collection("Users").findOne({ chat_id: chatId});
+    }
+
+    async updateUserByChatId(client, chatId, updatedUser) {
+        const result = await client.db("PMGQuizTelegramBotDB").collection("Users").updateOne({ chat_id: chatId }, { $set: updatedUser });
+    }
+
+    async createUser(client, newUser) {
+        await client.db("PMGQuizTelegramBotDB").collection("Users").insertOne(newUser);
+    }
 }
 
+let db = new DataBase();
+db.connect().catch(console.error);
+
+
 let answers = [];
-answers[0] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
-answers[1] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
-answers[2] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
-answers[3] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
+// answers[0] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
+// answers[1] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
+// answers[2] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
+// answers[3] = "Ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ ответ";
 
 let replyOptions = {
 
     reply_markup: {
         resize_keyboard: true,
-        one_time_keyboard: true,
-        inline_keyboard:[
-            [
-                { text: "A",  callback_data: "1"  },
-                { text: "B",  callback_data: "2"  },
-                { text: "C",  callback_data: "3"  },
-                { text: "D",  callback_data: "4"  }
-            ]
-        ]
+        one_time_keyboard: true
     },
 };
+
+
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', async msg => {
+
+    // also User ID that is passed to the database
     const chatId = msg.chat.id;
 
-    let questionNumber = 1;
+    let textFromUser = msg.text;
 
-    // send a message to the chat acknowledging receipt of their message
-    let questionFromDB = "Какой-то вопрос с базы данных. вопрос с базы данных. вопрос с базы данных. вопрос с базы данных. вопрос с базы данных."
-    questionFromDB += "\n";
+    if (textFromUser === "/start") {
+        let user = await db.findAUserByChatId(client, chatId);
 
-    bot.sendMessage(chatId, `Вопрос № ${questionNumber}\n ${questionFromDB}
-        [A]\n${answers[0]}\n
-        [B]\n${answers[1]}\n
-        [C]\n${answers[2]}\n
-        [D]\n${answers[3]}\n`,
-        replyOptions);
+        // @TODO check if user answered a question
+        // ...
 
-    console.log(msg);
+        // if user is null
+        if (!user) {
+            let newUser = {
+                chat_id: msg.chat.id,
+                first_name: msg.chat.first_name,
+                last_name: msg.chat.last_name,
+                username: msg.chat.username,
+                answers: {
+                    0: "incorrect",
+                },
+                correct_answers_cnt: 0
+            }
+
+            await db.createUser(client, newUser);
+        }
+
+        let questionNumber = 1;
+
+        // 20 october 2023
+        const originDate = 1697738400000;
+        const day = 86400000;
+        const date = new Date();
+        // here compare date
+        questionNumber = Math.ceil((date.getTime() - originDate) / day);
+
+        // @TODO compare last answerID with current question ID, if equals say "can't vote again"
+        if (user) {
+            // pull questionID to compare
+            // let lastQuestionId = 1;
+            // const lastQuestionId = Object.values(obj).pop();
+
+
+        }
+
+        let questionFromDB = await db.findOneQuestionById(client, questionNumber);
+        // console.log(questionFromDB);
+
+        answers = [];
+        questionFromDB.answers.forEach(a => {
+            answers.push(a);
+        })
+
+        let questionTextWithAnswersText = "";
+
+        // [A]\n
+        // ${answers[idx]}\n
+        delete replyOptions.reply_markup.inline_keyboard;
+        replyOptions.reply_markup.inline_keyboard = [];
+
+        answers.forEach((a, idx) => {
+           let letter = String.fromCharCode(idx + 65);
+           let answ = a.toString();
+
+           questionTextWithAnswersText += "[" + letter + "]\n" + answ + "\n\n";
+
+            // [
+            //     { text: "A",  callback_data: "1"  }
+            // ],
+           let tmpArrLetterForKeyboard = [
+               { text: letter,  callback_data: (idx + 1).toString() }
+           ];
+           replyOptions.reply_markup.inline_keyboard.push(tmpArrLetterForKeyboard);
+
+        });
+
+        await bot.sendMessage(chatId,
+            `Вопрос № ${questionNumber}\n\n${questionFromDB.text}\n\n${questionTextWithAnswersText}`,
+            replyOptions);
+    }
+
+    // console.log(msg);
 });
 
 bot.on("callback_query", async msg => {
